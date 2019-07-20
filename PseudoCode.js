11cbb65c07ea579e1ef587229c4d0f67515acf06
luke-du-hast <funkyseal@protonmail.com>
2019-07-20 14:09:29 +0200

    + First Commit

---
+++ PseudoCode.js
@@ -0,0 +1,42 @@
+//speudo code:
+
+//1. Get size of screen
+//- Make size fixed first to simplify
+//2. Get area of screen(length and breadth)
+//3. Find a size of square that matches nicely with the size of the screen
+//- Make the saure size fixed to simplify
+//4. Find number of columns and rows this size of square can fit into screen
+//- Tus finding the number of squares in total on the screen  
+//5. Create these squares from the top left of the screen moving right making a new column 
+// and propogate this for each new row to make a Grid
+//- If there is space left on th escreen then that is ok
+//6. Grid should be finished!
+
+
+
+class Screen: 
+Charactersitics:
+- Width 
+- Length
+- Grid
+
+
+class Square:
+Characteristics:
+- Has x and y position
+- width -  Float
+- Length = width - float
+- Has array of four sides 
+- Has array of 4 edges
+Features:
+
+
+class SquareGrid : inherits from Square
+Characteristics:
+- has itself a grid of 9 squares in side itself
+
+Classy Grid
+Characteristics
+- Array of Squares
+- Number of rows
+- Number of columns
