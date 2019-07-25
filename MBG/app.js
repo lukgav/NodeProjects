387276fd94c0c4fde1c8680e58d1742c628ef705
luke-du-hast <funkyseal@protonmail.com>
2019-07-25 12:38:02 +0200

    + implemented express, socket and am using canvas to start drawing objects, using node to keep track of players position

--- MBG/app.js
+++ MBG/app.js
@@ -1,14 +1,51 @@
 const express = require('express');
-const io = require('socket.io');
+//Set modules
 const path = require('path');
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../client')
+const serv = require('http').Server(app);
+const testIO = require('socket.io')(serv, {});
 
-app.use(express.static(publicDirectoryPath));
 
+
+//set local variables
+const publicDirectoryPath = path.join(__dirname, './client')
+const port = '2000';
+
+app.use(express.static(publicDirectoryPath));   
 app.get('', (req, res) =>{
+    res.get('index.html');
+});
+serv.listen(port, () => {
+    console.log('Server runnning on port: ' + port);
 });
 
-app.listen('8000', () => {
-    console.log('Server runnning on port 8000');
-});
+var SOCKET_LIST = {};
+testIO.sockets.on('connection', (socket) =>{
+    console.log('socket connection');
+
+    //player variables
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    SOCKET_LIST[socket.id] = socket;
+
+    socket.on('sad', (obj) =>{
+        console.log('sad because ' + obj.reason);
+    })
+
+    socket.emit('serverMsg', {
+        msg: 'hello socket',
+    })
+})
+
+setInterval(()=>{
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.x++;
+        socket.y++;
+        socket.emit('newPosition'),{
+            x:socket.x,
+            y:socket.y
+        }
+    }
+})
