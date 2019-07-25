bde414808e947aefbdb1b6f436beaf8c699e420e
luke-du-hast <funkyseal@protonmail.com>
2019-07-25 19:23:52 +0200

    + socket.io is being used to communicate between client and server

--- Grid/src/app.js
+++ Grid/src/app.js
@@ -1,73 +1,79 @@
 //require modules
 const path = require('path');
 const express = require('express');
+const app = express();
+const serve = require('http').Server(app);
 const hbs = require('hbs');
-// const jsdom  = require("jsdom");
-const { createCanvas, loadImage } = require('canvas');
-const fs = require('fs');
+const io = require('socket.io')(serve, { origins: '*:*' });
 
 //require other JS files
-const Square = require('./dist/SquareClass.js')
-
-// const canvas = document.getElementById('grid');
-
-
-//define vars from modules
-const app = express();
-const canvas = createCanvas(800, 600)
-const ctx = canvas.getContext('2d');
-
-
-ctx.font = '30px Impact'
-ctx.rotate(0.1)
-ctx.fillText('Hello Canvas!', 50, 100)
-ctx.save();
-
-
-// const { JSDOM } = jsdom;
-// const dom = new JSDOM("./templates/views/index.hbs");
-// console.log(dom.window.document.querySelector("p").textContent);
-
 //Set up paths
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
+
 // define express config paths
 app.use(express.static(publicDirectoryPath));
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
-hbs.registerPartials(partialsPath);
-
+app.use(express.static('public'));
+// app.set('view engine', 'hbs');
+// app.set('views', viewsPath);
+// hbs.registerPartials(partialsPath);
 //initialise classes
-var sqr = new Square(20, 20, 10, 10);
+const indexPath = (path.join(__dirname, '../public/index.html'));
+app.get('', (req, res) =>{
+    res.set('Content-Type', 'text/html');
+    res.set('X-Content-Type-Options', 'sniff');
 
-// app.get('', (req, res) => {
-//     res.send('Hello Express!');
+    res.sendFile(indexPath);
+})
+// app.get('/about', (req, res) => {
+//     res.render('index', {
+//         title: 'About Grids',
+//     })
 // })
 
-app.get('', (req, res) =>{
-    res.render('index', {
-        title: sqr.DrawMe(),
-        image: "/img/me.png",
-        name: 'CrabMan'
-    })
-})
+// app.get('/grid', (req, res) => {
+//     res.render('index', {
+//         title: 'No Grids found here :(',
+//     })
+// })
 
-console.log(sqr.sName)
-
-app.get('/about', (req, res) => {
-    res.render('index', {
-        title: 'About Grids',
-        video: "/img/warface.mp4",
-    })
-})
-
-app.get('/grid', (req, res) =>{
-    res.render('index', {
-        title: 'No Grids found here :(',
-    })
-})
-
-app.listen(3000, () =>{
+serve.listen(3000, () => {
     console.log('Server is on port 3000.');
 })
+
+ 
+var SOCKET_LIST = {};
+var socketTest;
+io.sockets.on('connection', (socket) =>{
+    console.log('socket connection');
+    // square variables
+    socket.x = 0;
+    socket.y = 0;
+    socket.id = Math.random();
+    socket.width = 100;
+
+    // SOCKET_LIST[socket.id] = socket;
+    socketTest = socket;
+    socket.on('sad', (data) =>{
+        console.log('sad because no square is drawn...' + data.reason);
+    })
+
+});
+
+const loopInterval = 1000/25;
+
+setInterval(()=> {
+    var pack = [];
+
+    for (var i in SOCKET_LIST) {
+        var socket = socketTest;
+        // console.log(i);
+        // console.log(socket.x);
+        // console.log(socket.y);
+        // console.log(socket.width);
+        // console.log(socket.x);
+        socket.emit('drawSquare', socket)
+
+    }
+}, loopInterval);
