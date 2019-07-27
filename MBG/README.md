d8b7f6b56c0b1ccd4d54eaac62c6f45824fcf5d2
crabman84 <lukegavin.5@outlook.com>
2019-07-27 15:01:38 +0200

    +b player and keyboard input however wrapper function isnot actig as anticipated

--- MBG/README.md
+++ MBG/README.md
@@ -2,10 +2,10 @@
 To make a grid in NodeJS so I can better make games in other projects.
 
 #git :
- +: Shows code was mainly added in its respective commit
- -: Shows that code was mainly deleted this commit
- w: Shows that something related to the commt has started/is WORKING
- b: Shows that something related to the commt has/is BROKEN
+ **+**: Shows code was mainly added in its respective commit
+ **-**: Shows that code was mainly deleted this commit
+ **w**: Shows that something related to the commt has started/is WORKING
+ **b**: Shows that something related to the commt has/is BROKEN
 
 
  
--- MBG/app.js
+++ MBG/app.js
@@ -19,41 +19,87 @@ serv.listen(port, () => {
     console.log('Server runnning on port: ' + port);
 });
 
+var PLAYER_LIST = {};
 var SOCKET_LIST = {};
+
+var Player = (id) =>{
+    var self = {
+        x: 250,
+        y: 250,
+        id: id,
+        number: "" + Math.floor(10 * Math.random()),
+        pressing: {
+            right: false,
+            left: false,
+            up: false,
+            down: false
+        },
+        maxSpeed: 2,
+
+    }
+
+
+    self.updatePosition = () => {
+        if(self.pressing.right){
+            self.x += self.maxSpeed;
+        }
+        if (self.pressing.left) {
+            self.x -= self.maxSpeed;
+        }
+        if (self.pressing.up) {
+            self.y -= self.maxSpeed;
+        }
+        if (self.pressing.down) {
+            self.y += self.maxSpeed;
+        }
+    }
+
+    return self;
+}
+ 
 testIO.sockets.on('connection', (socket) =>{
     console.log('socket connection');
 
     //player variables
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = "" + Math.floor(10 * Math.random());
     SOCKET_LIST[socket.id] = socket;
+
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
+    socket.on('keyPress', (data) =>{
+        if(data.inputId === 'right'){
+            player.pressing.right = data.state;
+        } 
+        else if (data.inputId === 'left') {
+            player.pressing.left = data.state;
+        } 
+        else if (data.inputId === 'down') {
+            player.pressing.down = data.state;
+        } 
+        else if (data.inputId === 'up') {
+            player.pressing.up = data.state;
+        } 
+    });
+
 
     socket.on('disconnect', () => {
         delete SOCKET_LIST[socket.id];
-    })
-
-    socket.on('sad', (obj) =>{
-        console.log('sad because ' + obj.reason);
-    })
-
-    socket.emit('serverMsg', {
-        msg: 'hello socket',
+        delete PLAYER_LIST[socket.id];
     })
 })
 
 const loopInterval = 10;
 setInterval( () => {
     var pack = [];
-    for(var i in SOCKET_LIST){
-        var socket = SOCKET_LIST[i];
-        socket.x++;
-        socket.y++;
+    for(var i in PLAYER_LIST){
+        var player = PLAYER_LIST[i];
+        // player.x++;
+        // player.y++;
+        player.updatePosition();
         pack.push({
-            x: socket.x,
-            y: socket.y,
-            number:socket.number,
+            x: player.x,
+            y: player.y,
+            number:player.number,
         })
     }
     for (var i in SOCKET_LIST) {
